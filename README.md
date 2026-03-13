5940799c312ec5232869670b9c96ff7c426564dc
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-13 15:04:50 +0530

    📜✨ add setup files, update readme notes for github, publish in pypi

--- README.md
+++ README.md
@@ -9,4 +9,23 @@
 **`APTracker`** is an advanced Python module for tracking multiple projects running on a single/distributed cluster using
 managers and/or objects to handle/track different projects.
 
+## Getting Started
+
+The project is available in [PyPI](https://pypi.org/p/aptracker) and can be installed using standard `pip` library or other
+default package managers as below:
+
+```shell
+pip install aptracker
+```
+
+## Contribution Guidelines
+
+All contributions, bug reports, bug fixes, documentation improvements, enhancements, and ideas are welcome. A detailed
+overview on how to contribute can be found in the **contributing guidelines**. If you run into an issue, please file a new
+[issue](https://github.com/PyUtility/apttracker/issues) for discussion. Create a pull request for a new feature or a fix to
+an existing issue [here](https://github.com/ZenithClown/aptracker/pulls).
+
+As contributors and maintainers to this project, you are expected to abide by [PyUtility](https://github.com/PyUtility)'s
+code of conduct. More information can be found at: **Contributor Code of Conduct**.
+
 </div>
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+
+sys.path.append(
+    os.path.join(os.path.dirname(__file__))
+)
+
+import aptracker
+
+setup(
+    name = "aptracker",
+    version = aptracker.__version__,
+    description = "Advanced Project Tracker and Manager Application",
+    long_description = open("README.md", "r").read(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/PyUtility/aptracker",
+    packages = find_packages(
+        exclude = ["tests*", "examples*"]
+    ),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires = ">=3.12"
+)
