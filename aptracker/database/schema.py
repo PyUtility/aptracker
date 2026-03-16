46a9c43cf73336b28171fa21a61434d8d9e49989
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-16 10:38:13 +0530

    ✨ added event logger table schema
    - event logger table can be used to capture any type of message from the project session runs
    - uses session id as the foreign key, other fields are self-explnatory

--- aptracker/database/schema.py
+++ aptracker/database/schema.py
@@ -12,7 +12,9 @@ collision when the module shares the database with other application.
 import datetime as dt
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime, Index, ForeignKey, Boolean
+from sqlalchemy import (
+    String, DateTime, Index, ForeignKey, Boolean, Integer
+)
 
 from aptracker._base.schema import BaseSchema
 
@@ -93,4 +95,45 @@ class SessionRecord(BaseSchema):
 
     decommissioned_by : Mapped[str] = mapped_column(
         String(36), nullable = True
+    )
+
+
+class EventLogs(BaseSchema):
+    """
+    ORM Mapping for the ``apt_events`` Table in a Relational DB
+    """
+
+    __tablename__ = "apt_events"
+    __table_args__ = (
+        Index("idx_apt_events_session_id", "session_id"),
+    )
+
+    event_id : Mapped[int] = mapped_column(
+        Integer, primary_key = True, autoincrement = True
+    )
+
+    session_id : Mapped[str] = mapped_column(
+        String(36),
+        ForeignKey("apt_session.session_id", ondelete = "CASCADE"),
+        nullable = False
+    )
+
+    message : Mapped[str] = mapped_column(
+        String(1024), nullable = False
+    )
+
+    # all the below are for exception handling, defaults to None
+    exception_type : Mapped[str] = mapped_column(
+        String(64), nullable = True
+    )
+    exception_value : Mapped[str] = mapped_column(
+        String(128), nullable = True
+    )
+    exception_tracebak : Mapped[str] = mapped_column(
+        String(1024), nullable = True
+    )
+
+    created_on : Mapped[dt.datetime] = mapped_column(
+        DateTime(timezone = True), nullable = False,
+        default = dt.datetime.now()
     )
